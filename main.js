4ae7acc8253db4cffa088f7890707b9011d1e602
Ppvc <118616495+Ppvc@users.noreply.github.com>
2023-09-25 16:20:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var first_name = "Pedro";
+var last_name = "Schiavo";
+var concat = first_name.concat(last_name);
+console.log(concat);
+
+var a = 5;
+var b = 9;
+var add = a+b;
+console.log(add);
+var potencia = Math.pow(5,9);
+console.log(potencia);
+var div = a/b;
+console.log(div);
+var multi = a*b;
+console.log(multi);
+var sub = b-a;
+console.log(sub);
+var numeroP= div.toFixed(2);
+console.log(numeroP);
